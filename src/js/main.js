584277f4992c444d8b1afca7e35df10a127f03c9
jonzielen <jzielenkievicz@nycgo.com>
2017-05-25 15:28:02 -0400

    add Quiz object

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,37 @@
 (function() {
-  const quizElem = document.querySelector('.quiz');
-  
-  console.log(quizElem);
+
+  const Quiz = {
+    newElem: function(elemType, className, text) {
+      let elem = document.createElement(elemType);
+
+      // adds class name if set
+      if (className !== undefined) elem.className = className;
+
+      // adds text if set
+      if (text !== undefined) elem.innerHTML = text;
+
+      return elem;
+    },
+    append: function(obj1, obj2) {
+      var selElem = [];
+
+      Array.prototype.slice.call(arguments).map(function(e) {
+        if (typeof e === 'object') selElem.push(e);
+        if (typeof e === 'string') selElem.push(document.querySelector(e));
+      });
+
+      return selElem[0].appendChild(selElem[1]);
+    }
+  };
+
+  const quizElem = document.querySelector('#quiz');
+  const qWrapper = Quiz.newElem('div', 'quiz-wrapper', 'qw');
+
+  //console.log(qWrapper);
+
+  Quiz.append('#quiz', qWrapper);
+
+  //console.log(quizElem);
 
 
 })();
